23faa4ddcc98e089158e27fae857ea42e1fe2644
Ingmar Kole <ikole@student.codam.nl>
2021-11-13 15:07:47 +0100

    Refactor: now the code is not completely F***ed

--- main.cpp
+++ main.cpp
@@ -3,95 +3,82 @@
 # include <string>
 # include <iomanip>
 
+# define MATRIX_SIZE 6
+# define FADE_SIZE 19
 
-void		set_color(char c, std::string& s)
+
+typedef int	color_matrix[MATRIX_SIZE][MATRIX_SIZE][MATRIX_SIZE];
+typedef int fade_matrix[FADE_SIZE][MATRIX_SIZE];
+
+static void	put_row_in_fade(size_t& i, size_t z, size_t y, fade_matrix fade, color_matrix matrix)
 {
-	s += c;
-	s += ';';
+	for (size_t x = 0; x < MATRIX_SIZE; x++) 
+		fade[i][x] = matrix[z][y][x];
+	i++;
 }
 
-/*
-** 38 to set foreground color;
-** 2 for rgb format;
-*/
-std::string	set_rgb(char r, char g, char b)
+static void	init_fade_reverse_y_axis(int color, size_t& i, fade_matrix fade, color_matrix matrix)
 {
-	std::string ret = "\e[38;2;";
-	set_color(r, ret);
-	set_color(g, ret);
-	set_color(b, ret);
-	ret += "m";
-	return (ret);
+	for (size_t z = 4; z > 0; z--)
+		put_row_in_fade(i, z, color, fade, matrix);
 }
 
-void	broken_rgb()
+static void	init_fade_y_axis(int color, size_t& i, fade_matrix fade, color_matrix matrix)
 {
-	for (size_t r = 0; r < 16; ++r) {
-		for (size_t g = 0; g < 16; ++g) {
-			for (size_t b = 0; b < 16; ++b) {
-				std::cout << set_rgb(r, g, b) \
-					<< "r: " << std::to_string(r) \
-					<< " g: " << std::to_string(g) \
-					<< " b: " << std::to_string(b) \
-					<< std::endl;			
-			}
-		}
-	}
+	for (size_t z = 1; z < 5; z++)
+		put_row_in_fade(i, z, color, fade, matrix);
 }
 
-void	working_colors()
+static void	init_fade_z_axis(int color, size_t& i, fade_matrix fade, color_matrix matrix)
 {
-	// char fade[] = {17, 18, 19, 20, 21, 26, 27};
-	int WTF[6][6][6];
+	for (size_t y = 0; y < MATRIX_SIZE; y++)
+		put_row_in_fade(i, color, y, fade, matrix);
+}
 
+static void	init_fade_reverse_z_axis(int color, size_t& i, fade_matrix fade, color_matrix matrix)
+{
+	for (size_t y = 5; y > 0; y--)
+		put_row_in_fade(i, color, y, fade, matrix);
+}
+
+static void	make_fade_matrix(fade_matrix fade, color_matrix matrix)
+{
+	size_t i = 0;
+
+	init_fade_z_axis(5, i, fade, matrix);
+	init_fade_reverse_y_axis(5, i, fade, matrix);
+	init_fade_reverse_z_axis(0, i, fade, matrix);
+	init_fade_y_axis(0, i, fade, matrix);
+	
+	// for (size_t l = 0; l < FADE_SIZE; l++) {
+	// 	for (size_t u = 0; u < MATRIX_SIZE; u++) {
+	// 		std::cout << "\e[38;5;" << fade[l][u] << "m" << "pog" << " ";
+	// 	}
+	// 	std::cout << std::endl;
+	// }
+}
+
+
+void	colors_init(color_matrix matrix)
+{
+	/* skip first 16 colors */
 	int color = 16;
-	for (size_t z = 0; z < 6; ++z) {
-		for (size_t y = 0; y < 6; ++y) {
-			for (size_t x = 0; x < 6; ++x) {
-				WTF[z][y][x] = color;
-				// std::cout << "\e[38;5;" << color << "m" << color << " ";
+
+	for (size_t z = 0; z < MATRIX_SIZE; ++z) {
+		for (size_t y = 0; y < MATRIX_SIZE; ++y) {
+			for (size_t x = 0; x < MATRIX_SIZE; ++x) {
+				matrix[z][y][x] = color;
 				color++;
 			}
-			// std::cout << std::endl;
 		}
 	}
-
-	int fade_color = 5;
-	for (size_t y = 0; y < 6; y++) {
-		for (size_t x = 0; x < 6; x++) {
-			std::cout << "\e[38;5;" << WTF[fade_color][y][x] << "m" << "pog" << " ";
-		}
-		std::cout << std::endl;
-	}
-	for (size_t z = 4; z > 0; z--) {
-		for (size_t x = 0; x < 6; x++) {
-			std::cout << "\e[38;5;" << WTF[z][fade_color][x] << "m" << "pog" << " ";
-		}
-		std::cout << std::endl;
-	}
-	fade_color = 0;
-	for (size_t y = 5; y > 0; y--) {
-		for (size_t x = 0; x < 6; x++) {
-			std::cout << "\e[38;5;" << WTF[fade_color][y][x] << "m" << "pog" << " ";
-		}
-		std::cout << std::endl;
-	}
-	for (size_t z = 1; z < 5; z++) {
-		for (size_t x = 0; x < 6; x++) {
-			std::cout << "\e[38;5;" << WTF[z][fade_color][x] << "m" << "pog" << " ";
-		}
-		std::cout << std::endl;
-	}
-	
-	// for (size_t i = 1; i + 15 < 232; ++i) {
-	// 	std::cout << "\e[38;5;" << i + 15 << "m" << i + 15 << " ";
-	// 	if (i % 6 == 0)
-	// 		std::cout << std::endl;
-	// }
 }
 
 int main(void)
 {
-	// broken_rgb();
-	working_colors();
+	color_matrix	matrix;
+	fade_matrix		fade;
+
+	colors_init(matrix);
+	make_fade_matrix(fade, matrix);
 }
