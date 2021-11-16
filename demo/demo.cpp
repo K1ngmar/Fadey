80836dca839cf33e5192c28e5ea1d41b8435d0da
Ingmar Kole <ikole@student.codam.nl>
2021-11-16 19:39:57 +0100

    Feat: added a demo for fadey

---
+++ demo/demo.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*       _     _      _     _      _     _      _     _      _     _          */
+/*      (c).-.(c)    (c).-.(c)    (c).-.(c)    (c).-.(c)    (c).-.(c)         */
+/*       / ._. \      / ._. \      / ._. \      / ._. \      / ._. \          */
+/*     __\( Y )/__  __\( Y )/__  __\( Y )/__  __\( Y )/__  __\( Y )/__        */
+/*    (_.-/'-'\-._)(_.-/'-'\-._)(_.-/'-'\-._)(_.-/'-'\-._)(_.-/'-'\-._)       */
+/*       || F ||      || A ||      || D ||      || E ||      || Y ||          */
+/*     _.' `-' '._  _.' `-' '._  _.' `-' '._  _.' `-' '._  _.' `-' '._        */
+/*    (.-./`-'\.-.)(.-./`-'\.-.)(.-./`-'\.-.)(.-./`-'\.-.)(.-./`-'\.-.)       */
+/*     `-'     `-'  `-'     `-'  `-'     `-'  `-'     `-'  `-'     `-'        */
+/*                                                                            */
+/*                   Kingmar  |  https://github.com/K1ngmar                   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fadey.hpp>
+#include <sstream>
+#include <fstream>
+
+int main()
+{
+	std::ifstream file("demo/BANNER");
+	std::stringstream buffer;
+	buffer << file.rdbuf();
+	km::fadey << buffer.str() << std::endl;
+
+	km::fadey << "Every time you seed fady the fade pattern will change :0" << std::endl;
+
+	km::fadey << "Fadey supports a couple ways of fadifying your output :0" << std::endl;
+
+	km::fadey << "it can also print " << 42069 << std::endl; 
+	
+	return (0);
+}
